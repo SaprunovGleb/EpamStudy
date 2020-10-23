5a34442d52f318135df232a710040de0c4c428e4
SaprunovGleb <72560484+SaprunovGleb@users.noreply.github.com>
2020-10-23 14:51:40 +0300

    Add files via upload

--- changeCopyToLink.py
+++ changeCopyToLink.py
@@ -2,67 +2,89 @@
 """
 Created on Wed Oct  7 18:18:34 2020
 
-@author: Flyin
+@author: Saprunov Gleb
+
+version 2.0
 """
 import os
+import sys
 
-def makeListOfFiles(directory,massFiles):
+def make_list_of_files(directory,arrFiles):
 
     files = os.listdir(directory) 
     #print(files)
     for file in files:
+        #Make full path for each file and directory
         filePath = directory + "\\" + file
         if os.path.isdir(filePath):
             #print("Директория ", filePath)
-            makeListOfFiles(filePath,massFiles)
+            #For directory use recursion 
+            make_list_of_files(filePath,arrFiles)
         elif os.path.isfile(filePath):
             #print("Файл ", filePath)
-            massPromFile = []
-            massPromFile.append(filePath)
-            massPromFile.append(os.path.getsize(filePath))
-            #print(massPromFile)
-            massFiles.append(massPromFile)
+            #For each file make array and put there full path and size
+            arrPromFile = [filePath, os.path.getsize(filePath)]
+            #print(arrPromFile)
+            #This array add to array of all files
+            arrFiles.append(arrPromFile)
         
-def linkCopy(massFiles):
-    #print (massFiles)
+def link_copy(arrFiles):
+    #print (arrFiles)
     i=0
-    massLinkedFiles=[]
-    while i < (len(massFiles)-1):
+    arrLinkedFiles=[]
+    #Here we take each file in arrFiles in sequence.
+    while i < (len(arrFiles)-1):
         j=1
-        while (j+i)<len(massFiles):
-            if massFiles[i][1]==massFiles[i+j][1]:
-                fileI= open(massFiles[i][0],"rb")
-                fileJ= open(massFiles[i+j][0],"rb")
+        #Here we take each file further on the array
+        while (j+i)<len(arrFiles):
+            #If size is the same, open files to compare
+            if arrFiles[i][1] == arrFiles[i+j][1]:
+                fileI = open(arrFiles[i][0],"rb")
+                fileJ = open(arrFiles[i+j][0],"rb")
                 #print(fileI.read())
                 #print(fileJ.read())
+                #If they are the same we change i+j file to link
                 if fileI.read()==fileJ.read():
                     fileJ.close()
-                    os.remove(massFiles[i+j][0])
-                    os.link(massFiles[i][0],massFiles[i+j][0])
-                    massLinkedFiles.append(massFiles[i+j][0])
-                    massFiles.pop(i+j)
+                    os.remove(arrFiles[i+j][0])
+                    os.link(arrFiles[i][0],arrFiles[i+j][0])
+                    #We remove this file out the arrFiles
+                    arrFiles.pop(i+j)
+                    #We add fule path of link to array
+                    arrLinkedFiles.append(arrFiles[i+j][0])
                 else:
-                    j+=1
+                    #Here we add 1, because we need to change 2 file.
+                    #If 2 file is the same of the 1, his path get away from
+                    #array, else number of 2 file in array grows on 1.
+                    j += 1
                 fileI.close()
                 fileJ.close()
+            #If size not the same stop stop taking the following files
             else:
                 break
-        i+=1
-    print(massLinkedFiles,len(massLinkedFiles))
+        i += 1
+    #We show added links, and amount of them.
+    print(arrLinkedFiles,len(arrLinkedFiles))
                 
                 
                 
                 
-def changeCopyToLink(directory="D:\Testdir"):
+def change_copy_to_link(directory="D:\Testdir"):
     if os.path.isdir(directory):
-        massFiles=[]
-        makeListOfFiles(directory,massFiles)
-        massFiles.sort(key = lambda x: x[1])
-        linkCopy(massFiles)
+        #Make array for full paths files
+        arrFiles=[]
+        make_list_of_files(directory,arrFiles)
+        #Sort array by size o files
+        arrFiles.sort(key = lambda x: x[1])
+        #Link copies
+        link_сopy(arrFiles)
     else:
         print("Передайте значение существующей директории")
 
-
+if __name__ == '__main__':
+    # With the argument you should give full path to directory,  
+    # within which all the same files will be linked, including subfolders
+    change_copy_to_link(sys.argv[1])
 
 
 #changeCopyToLink("D:\Testdir")
